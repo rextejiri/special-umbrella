8938c29d1abd56bc0da0b5ad580c19694a9c9803
Tejiri Ibihwiori <tejiri@Tejiris-MacBook-Pro.local>
2021-01-16 23:53:01 -0600

    user can create post

--- public/js/app.js
+++ public/js/app.js
@@ -5,18 +5,23 @@ class App extends React.Component {
 		image: "",
 		caption: "",
 		likes: 0,
+		posts: []
 
 	}
 	handleChange = (event) => {
      this.setState({
        [event.target.id]: event.target.value,
      })
-   }
+  }
 
-	 handleSubmit = (event) => {
-     event.preventDefault()
-     axios.post('/home', this.state).then((response) => console.log(response))
-   }
+  handleSubmit = (event) => {
+    event.preventDefault()
+    axios
+      .post('/home', this.state)
+      .then((response) =>
+        this.setState({ posts: response.data, image: '', username: '' ,caption: "",profileImage: "", likes: ""})
+      )
+  }
 
 
 	render = () => {
@@ -30,31 +35,52 @@ class App extends React.Component {
 						<img src="" alt="share"/>
 					</div>
 				</header>
-				<div className="post-container">
-					<div className="profile-div" >
-						<img src="" alt="profile image"/>
-						<h5>Username</h5>
-						<img src="" alt="edit" />
-					</div>
-					<div className="posted-image">
-						<img src="" alt="post" />
-					</div>
-					<div className="like-bar">
-						<button>
-						Like
-						</button>
-						<button>
-						comment
-						</button>
-						<button>
-						share
-						</button>
-						<button>
-						Delete
-						</button>
-					</div>
-				</div>
+				{this.state.posts.map((post) => {
+					return (
+						<div className="post-container">
+							<div className="profile-div" >
+									<img src="" alt="profile image"/>
+									<h5>Username</h5>
+									<img src={post.profileImage} alt={post.name} />
+							</div>
+							<div className="posted-image">
+									<img src={post.image} alt={post.username} />
+							</div>
+							<div className="like-bar">
+								<button>
+								Like
+								</button>
+								<button>
+								comment
+								</button>
+								<button>
+								share
+								</button>
+								<button>
+								Delete
+							</button>
+							</div>
+						</div>
+					)
+				})}
+
 				<footer>
+					<form onSubmit={this.handleSubmit}>
+						<label htmlFor="caption">Caption</label>
+						<input type="text" id="caption" onChange={this.handleChange}/>
+						<br />
+						<label htmlFor="image">Image</label>
+						<input type="text" id="image" onChange={this.handleChange}/>
+						<br />
+						<label htmlFor="profileImage">profileImage</label>
+						<input type="text" id="profileImage" onChange={this.handleChange}/>
+						<br />
+						<label htmlFor="username">Username</label>
+						<input type="text" id="username" onChange={this.handleChange}/>
+						<br />
+						<input type="submit" value="Post" />
+
+					</form>
 					<div className="footer-div">
 					<button>
 					Home
